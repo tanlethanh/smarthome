00a9f86d518bdfa43af5323d3cc9f82f75214bf9
Tan Le <tanlethanh8266@gmail.com>
2023-03-09 11:14:49 +0700

    Hidden header shown of navigation

--- mobile-ui/App.js
+++ mobile-ui/App.js
@@ -2,16 +2,22 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { HomeScreen, DetailsScreen } from "./screens";
+import { HomeScreen, DetailsScreen, LoginScreen } from "./screens";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Home">
+            <Stack.Navigator
+                initialRouteName="Login"
+                screenOptions={{
+                    headerShown: false
+                }}
+            >
                 <Stack.Screen name="Home" component={HomeScreen} />
                 <Stack.Screen name="Details" component={DetailsScreen} />
+                <Stack.Screen name="Login" component={LoginScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
